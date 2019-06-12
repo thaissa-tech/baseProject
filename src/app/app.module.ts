78ca6e00adeaf13bcf60c208468a7cadc1403ca8
Guimarães Campos <a.guimaraes.campos@accenture.com>
2019-06-11 21:20:40 -0300

    feat(base): base component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppBaseComponent } from './base/base.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppBaseComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppBaseComponent]
 })
 export class AppModule { }
--- src/app/base/base.component.spec.ts
+++ src/app/base/base.component.spec.ts
@@ -1,33 +1,33 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+import { AppBaseComponent } from './base.component';
 
-describe('AppComponent', () => {
+describe('AppBaseComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
         RouterTestingModule
       ],
       declarations: [
-        AppComponent
+        AppBaseComponent
       ],
     }).compileComponents();
   }));
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture = TestBed.createComponent(AppBaseComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
 
   it(`should have as title 'project-base'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture = TestBed.createComponent(AppBaseComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('project-base');
   });
 
   it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture = TestBed.createComponent(AppBaseComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('h1').textContent).toContain('Welcome to project-base!');
